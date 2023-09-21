ce839a052e782a1ac4579d866c8f0715d0fc0780
Trollope91 <ljtdev91@gmail.com>
2023-09-21 05:00:37 +0000

    Add ability to switch from debug to production

--- app/app.py
+++ app/app.py
@@ -39,4 +39,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5200")),
-        debug=True)
+        debug=os.getenv('DEBUG')
+    )
