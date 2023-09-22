864e01a6b2d19c616d65e7e8287bdeccc4319b3e
Trollope91 <ljtdev91@gmail.com>
2023-09-22 05:24:21 +0000

    Login required pep 8

--- app/decorators/login_required.py
+++ app/decorators/login_required.py
@@ -1,17 +1,13 @@
 from functools import wraps
 from flask import request, redirect, url_for, session
 
-def login_required(func):
-    """
-    A decorator for protecting routes that require user authentication.
 
-    """
+def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if 'username' in session:
             return func(*args, **kwargs)
         else:
             return redirect(url_for('login_bp.login'))
-    
-    return wrapper
 
+    return wrapper
